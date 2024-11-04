ed0ecbbb8daac9f667284467088f49d5f4ce332f
nethen <sky08ninja@gmail.com>
2024-11-04 21:15:08 +0300

    rewrote brush with screenpass

--- src/brush/mod.rs
+++ src/brush/mod.rs
@@ -1,3 +1,4 @@
+use crate::screenpass::{self, ScreenPass};
 use egui_wgpu::wgpu;
 
 #[repr(C)]
@@ -11,10 +12,8 @@ struct RawUniformData {
 }
 
 pub struct Brush {
+    screenpass: ScreenPass,
     uniform_buffer: wgpu::Buffer,
-    uniform_bind_group: wgpu::BindGroup,
-    pipeline_layout: wgpu::PipelineLayout,
-    shader_module: wgpu::ShaderModule,
 }
 
 impl Brush {
@@ -26,126 +25,57 @@ impl Brush {
             mapped_at_creation: false,
         });
 
-        let uniform_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("brush uniform bind group layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("brush uniform bind group"),
-            layout: &uniform_bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: uniform_buffer.as_entire_binding(),
-            }],
-        });
-
-        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: Some("brush render pipeline layout"),
-            bind_group_layouts: &[&uniform_bind_group_layout],
-            push_constant_ranges: &[],
-        });
-
+        let bind_group_layout_binding_types = vec![vec![wgpu::BindingType::Buffer {
+            ty: wgpu::BufferBindingType::Uniform,
+            has_dynamic_offset: false,
+            min_binding_size: None,
+        }]];
         let shader_module = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
-        Brush {
-            uniform_buffer,
-            uniform_bind_group,
-            pipeline_layout,
-            shader_module,
-        }
-    }
+        let screenpass = ScreenPass::new(device, bind_group_layout_binding_types, shader_module);
 
-    fn create_render_pipeline(
-        &self,
-        device: &wgpu::Device,
-        texture_format: wgpu::TextureFormat,
-    ) -> wgpu::RenderPipeline {
-        device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("brush render pipeline"),
-            layout: Some(&self.pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &self.shader_module,
-                entry_point: "vs_main",
-                buffers: &[],
-                compilation_options: Default::default(),
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &self.shader_module,
-                entry_point: "fs_main",
-                compilation_options: Default::default(),
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: texture_format,
-                    blend: Some(wgpu::BlendState::PREMULTIPLIED_ALPHA_BLENDING),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleStrip,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: None,
-                unclipped_depth: false,
-                polygon_mode: wgpu::PolygonMode::Fill,
-                conservative: false,
-            },
-            depth_stencil: None,
-            multiview: None,
-            multisample: Default::default(),
-        })
+        Self {
+            screenpass,
+            uniform_buffer,
+        }
     }
 
     pub fn draw(
         &self,
         device: &wgpu::Device,
         queue: &wgpu::Queue,
-        texture: &wgpu::Texture,
+        out_texture: &wgpu::Texture,
         color: [f32; 3],
         pos: [f32; 2],
         radius: f32,
     ) {
-        let render_pipeline = self.create_render_pipeline(&device, texture.format());
-        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
         let uniform_data = RawUniformData {
             color,
             radius,
-            texture_size: [texture.width() as f32, texture.height() as f32],
+            texture_size: [out_texture.width() as f32, out_texture.height() as f32],
             pos,
         };
         queue.write_buffer(&self.uniform_buffer, 0, bytemuck::bytes_of(&uniform_data));
 
-        let mut encoder = device.create_command_encoder(&Default::default());
-
-        {
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("brush render pass"),
+        self.screenpass
+            .render(&screenpass::ScreenPassRenderDescriptor {
+                device: &device,
+                queue: &queue,
+                out_texture,
+                fragment_targets: &[Some(wgpu::ColorTargetState {
+                    format: out_texture.format(),
+                    blend: Some(wgpu::BlendState::PREMULTIPLIED_ALPHA_BLENDING),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                bind_group_resources: &[&[self.uniform_buffer.as_entire_binding()]],
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
+                    view: &out_texture.create_view(&wgpu::TextureViewDescriptor::default()),
+                    resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Load,
                         store: wgpu::StoreOp::Store,
                     },
-                    resolve_target: None,
                 })],
-                ..Default::default()
-            });
-
-            render_pass.set_pipeline(&render_pipeline);
-            render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
-            render_pass.draw(0..4, 0..1);
-        }
-
-        queue.submit(Some(encoder.finish()));
+            })
     }
 }
